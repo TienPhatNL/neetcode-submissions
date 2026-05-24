6216c10051a904249711d51200eba61fcecf67d9
Nguyễn Lê Tiến Phát <letienphat03@gmail.com>
2026-05-24 14:00:41 +0700

    Add: anagram-groups - submission-0

---
+++ Data Structures & Algorithms/anagram-groups/submission-0.cs
@@ -0,0 +1,24 @@
+public class Solution {
+    public List<List<string>> GroupAnagrams(string[] strs) {
+        Dictionary<string, List<string>> di = new Dictionary<string, List<string>>();
+        List<List<string>> result = new List<List<string>>();
+
+        foreach(string s in strs){
+            char[] chars = s.ToCharArray();
+            Array.Sort(chars);
+            string key = new string(chars);
+
+            if(di.ContainsKey(key)){
+                di[key].Add(s);
+            }else{
+                di.Add(key, new List<string>{s});
+            }
+        }
+
+        foreach(var items in di){
+            result.Add(items.Value);
+        }
+
+        return result;
+    }
+}
