662d82a9b07ccc7a6b4fdc53ed4775a47a6f2c92
Nguyễn Lê Tiến Phát <letienphat03@gmail.com>
2026-05-24 10:20:12 +0700

    Add: is-anagram - submission-2

---
+++ Data Structures & Algorithms/is-anagram/submission-2.cs
@@ -0,0 +1,22 @@
+public class Solution {
+    public bool IsAnagram(string s, string t) {
+        if(s.Length != t.Length) return false;
+
+        Dictionary<char, int> di = new Dictionary<char, int>();
+
+        foreach(char i in s){
+            if(di.ContainsKey(i)) di[i]++;
+            else di[i] = 1;
+        }
+
+        foreach(char i in t){
+            if(!di.ContainsKey(i)) return false;
+
+            else if(di[i] != 1) di[i]--;
+
+            else if(di[i] == 1) di.Remove(i);
+        }
+
+        return true;
+    }
+}
