1a349168c25604b31e26df1852671e643af76ead
Nguyễn Lê Tiến Phát <letienphat03@gmail.com>
2026-06-20 20:15:06 +0700

    Add: is-palindrome - submission-5

---
+++ Data Structures & Algorithms/is-palindrome/submission-5.cs
@@ -0,0 +1,15 @@
+public class Solution {
+    public bool IsPalindrome(string s) {
+        if(String.IsNullOrEmpty(s)) return false;
+
+        s = Regex.Replace(s, "[^a-zA-Z0-9]", "");
+        s = s.Trim().ToLower();
+        int right = s.Length - 1;
+        for(int left = 0; left < s.Length; left++){
+            if(s[left] != s[right]) return false;
+
+            right--;
+        }
+        return true;
+    }
+}
